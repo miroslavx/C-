615bf5cea7a18d3cada42722a7e91855f8802260
miroslavx <56218543+miroslavx@users.noreply.github.com>
2025-04-09 17:38:41 +0300

    Add files via upload

---
+++ ConsoleApp2/Class1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Kordamine
+{
+    class Startclass
+    {
+        public static void Main(string[] args)
+        {
+            // Tervitus
+            Console.WriteLine("Tere tulemast!");
+
+            // Nime küsimine
+            Console.Write("Sisesta oma nimi: ");
+            string eesnimi = Console.ReadLine();
+            Console.WriteLine("Tere, " + eesnimi);
+
+            // Arvude küsimine
+            Console.Write("Sisesta esimene arv: ");
+            int arv1 = int.Parse(Console.ReadLine());
+
+            Console.Write("Sisesta teine arv: ");
+            int arv2 = int.Parse(Console.ReadLine());
+
+            // Arvutamine ja tulemuse kuvamine
+            Console.WriteLine("Arvude {0} ja {1} korrutis võrdub {2}", arv1, arv2, arv1 * arv2);
+
+            // Ootab kasutaja sisendit enne programmi lõpetamist
+            Console.WriteLine("Vajuta Enter klahvi programmi lõpetamiseks...");
+            Console.ReadLine();
+        }
+    }
+}
